93a25f0964554e642ffc2912e591151d8800e4b7
wi22b052 <wi22b052@technikum-wien.at>
2023-06-19 20:52:23 +0200

    initial commit

---
+++ src/test/java/com/example/slmmaintenancemonitor/SlmMaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmmaintenancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmMaintenanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
